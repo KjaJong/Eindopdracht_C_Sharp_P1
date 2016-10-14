2b2a6effa17aa57782c64fa8d616977dca71e57f
HP_FOLIO9470M\gijsb <gijs.brian.joost@hotmail.com>
2016-10-14 09:56:37 +0200

    Pulling some shizzeru

--- ChoHan/ClientHandler.cs
+++ ChoHan/ClientHandler.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
+using System.Runtime.Remoting.Messaging;
 using SharedUtilities;
 
 namespace ChoHan
 {
     public class ClientHandler
     {
-        private Dictionary<TcpClient, int> _dictionary;
+        private readonly Dictionary<TcpClient, int> _dictionary;
 
         public ClientHandler(Dictionary<TcpClient, int> dictionary)
         {
@@ -82,8 +83,43 @@ namespace ChoHan
             list.Sort(
                 (pair1, pair2) => pair1.Value.CompareTo(pair2.Value)
             );
-            //TODO: Has to work for more clients.
 
+            bool playerOneWin = true;
+
+            foreach (var c in list)
+            {
+                if (c.Equals(list.ElementAt(0)))
+                {
+                     return;
+                }
+
+                switch (c.Value - list.ElementAt(0).Value)
+                {
+                    case 1:
+                        Console.WriteLine("Something went terribly wrong here");
+                        break;
+                    case 0:
+                        SharedUtil.SendMessage(c.Key, "You tied");
+                        playerOneWin = false;
+                        break;
+                    case -1:
+                        SharedUtil.SendMessage(c.Key, "You lose");
+                        break;
+                    default:
+                        Console.WriteLine("ffs are you here?!");
+                        break;
+                }
+                    
+            }
+
+            if (playerOneWin)
+            {
+                SharedUtil.SendMessage(list.ElementAt(0).Key, "You win");
+            }
+            else
+            {
+                SharedUtil.SendMessage(list.ElementAt(0).Key, "You lose");
+            }
 
 
             //TODO also needs reworking. The room doesn't play with one player and only closes when the server shuts off.
