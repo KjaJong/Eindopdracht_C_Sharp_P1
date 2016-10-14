ffbb72d4a0e3e73ca0215a20b1e7b90c57d4917d
HP_FOLIO9470M\gijsb <gijs.brian.joost@hotmail.com>
2016-10-14 13:49:02 +0200

    BOOLEANS

--- ChoHanClient/Client.cs
+++ ChoHanClient/Client.cs
@@ -50,38 +50,45 @@ namespace ChoHanClient
         {
             bool done = false;
             string message;
+            bool beginConfirm = false;
             List<string> messages = new List<string>();
             while (!done)
             {
                 if (!form.ConfirmAnswer)
                 {
                     SharedUtil.SendMessage(client, form.ConfirmAnswer.ToString());
+                    beginConfirm = true;
                 }
 
-                switch (SharedUtil.ReadMessage(client))
-                    { 
-                    case "give/answer":
-                        SharedUtil.SendMessage(client, form.Answer.ToString());
-                        break;
-                    //TODO voeg acties en andere cases toe om te de GUI te beinvloeden
-                    case "recieve/answer":
-                        Console.WriteLine(SharedUtil.ReadMessage(client));
-                        string rightAnswer = SharedUtil.ReadMessage(client);
-                        string score = SharedUtil.ReadMessage(client);
-                        form.Update(rightAnswer, score);
+
+                if (beginConfirm)
+                {
+                    switch (SharedUtil.ReadMessage(client))
+                    {
+                        case "give/answer":
+                            SharedUtil.SendMessage(client, form.Answer.ToString());
+                            break;
+                        //TODO voeg acties en andere cases toe om te de GUI te beinvloeden
+                        case "recieve/answer":
+                            Console.WriteLine(SharedUtil.ReadMessage(client));
+                            string rightAnswer = SharedUtil.ReadMessage(client);
+                            string score = SharedUtil.ReadMessage(client);
+                            form.Update(rightAnswer, score);
                             //TODO check if the read message give back useable data
-                        break;
-                    case "recieve/answer/final":
+                            break;
+                        case "recieve/answer/final":
                             form.UpdateMessageLabel(SharedUtil.ReadMessage(client));
+                            beginConfirm = false;
                             //TODO check if the read message give back useable data
-                        break;
-                    case "closing":
-                        message = SharedUtil.ReadMessage(client);
-                        break;
-                    default:
-                        Console.WriteLine("OI, The fuck you doing here m8");
-                        break;
-                     }
+                            break;
+                        case "closing":
+                            message = SharedUtil.ReadMessage(client);
+                            break;
+                        default:
+                            Console.WriteLine("OI, The fuck you doing here m8");
+                            break;
+                    }
+                }
             }
         }
 
