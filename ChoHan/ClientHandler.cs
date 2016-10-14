bdbd4853cb2ee64909b9f357c2cbf1735259a471
HP_FOLIO9470M\gijsb <gijs.brian.joost@hotmail.com>
2016-10-14 09:43:13 +0200

    God bless the dictionary

---
+++ ChoHan/ClientHandler.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using SharedUtilities;
+
+namespace ChoHan
+{
+    public class ClientHandler
+    {
+        private Dictionary<TcpClient, int> _dictionary;
+
+        public ClientHandler(Dictionary<TcpClient, int> dictionary)
+        {
+
+            _dictionary = dictionary;
+        }
+
+        private void StartGame(Dictionary<TcpClient, int> players)
+        {
+            List<string> answers = new List<string>();
+            int roundCount = 0;
+            ChoHan game = new ChoHan();
+
+            while (roundCount > 5)
+            {
+
+                int answercount = 0;
+                //Waits for every client to choose a answer
+                while (answercount != players.Count)
+                {
+                    answercount = 0;
+                    foreach (var c in players)
+                    {
+                        if (SharedUtil.ReadMessage(c.Key).Contains("True"))
+                        {
+                            answercount += 1;
+                        }
+                    }
+                }
+                //TODO Displays the result of the throw and annouces win or lose.
+                //send every client a message that they can send their answer
+                game.ThrowDice();
+                foreach (var c in players)
+                {
+                    SharedUtil.SendMessage(c.Key, "1");
+                    string answer = (SharedUtil.ReadMessage(c.Key));
+                    string[] message;
+                    if (answer.Equals("True"))
+                    {
+                        if (game.CheckResult(true))
+                        {
+                            _dictionary[c.Key] +=  1;
+                            message = new[] { c.Value.ToString(), "True" };
+                        }
+                        else
+                        {
+                            message = new[] { c.Value.ToString(), "False" };
+                        }
+                        SharedUtil.SendMessages(c.Key, message);
+                    }
+                    else
+                    {
+                        if (game.CheckResult(false))
+                        {
+                            _dictionary[c.Key] += 1;
+                            message = new[] { c.Value.ToString(), "True" };
+                        }
+                        else
+                        {
+                            message = new[] { c.Value.ToString(), "False" };
+                        }
+                        SharedUtil.SendMessages(c.Key, message);
+                    }
+
+
+                }
+                roundCount++;
+            }
+
+            List<KeyValuePair<TcpClient, int>> list = _dictionary.ToList();
+            list.Sort(
+                (pair1, pair2) => pair1.Value.CompareTo(pair2.Value)
+            );
+            //TODO: Has to work for more clients.
+
+
+
+            //TODO also needs reworking. The room doesn't play with one player and only closes when the server shuts off.
+            foreach (var c in players)
+            {
+                SharedUtil.WriteTextMessage(c.Key, "Thanks for playing.");
+                c.Key.Close();
+            }
+        }
+
+        public void HandleClientThread()
+        {
+            StartGame(_dictionary);
+            Console.WriteLine("Connection closed");
+        }
+    }
+}
--- ChoHan/Server.cs
+++ ChoHan/Server.cs
@@ -11,19 +11,20 @@ using System.Threading.Tasks;
 
 namespace ChoHan
 {
-    class Server : IComparer<int>
+    class Server
     {
         //TODO maybe start tracking points in the player and write the players of a session to json.
         //TODO negate every error, we'll work on it
         
         private IPAddress _currentId;
         private TcpListener _listner;
+        private List<ClientHandler> _handlers;
         
 
         public Server()
         {
             IPAddress localIP = GetLocalIpAddress();
-            
+            _handlers = new List<ClientHandler>();
 
             bool IpOk = IPAddress.TryParse(localIP.ToString(), out _currentId);
             if (!IpOk)
@@ -32,129 +33,33 @@ namespace ChoHan
                 Environment.Exit(1);
             }
 
-            TcpListener listner = new TcpListener(_currentId, 1337);
-            listner.Start();
+            TcpListener listener = new TcpListener(_currentId, 1337);
+            listener.Start();
 
             while (true)
             {
-                Thread thread = new Thread(HandleClientThread);
-                thread.Start(CheckForPlayers(listner));
+                ClientHandler handler = new ClientHandler(CheckForPlayers(listener));
+                Thread thread = new Thread(handler.HandleClientThread);
+                thread.Start();
+                _handlers.Add(handler);
             }
         }
-
-        private List<TcpClient> CheckForPlayers(TcpListener listner)
+        private Dictionary<TcpClient, int> CheckForPlayers(TcpListener listner)
         {
             //TODO it now uses two players. Maybe add more (like a room of eight?)
 
-            List<TcpClient> _activeClients = new List<TcpClient>();
+            Dictionary<TcpClient, int> _activeClients = new Dictionary<TcpClient, int>();
             while (_activeClients.Count != 2)
             {
                 Console.WriteLine("Waiting for player1");
                 TcpClient client = listner.AcceptTcpClient();
-                _activeClients.Add(client);
+                _activeClients.Add(client, 0);
             }
 
             return _activeClients;
         }
 
-        private void StartGame(List<TcpClient> players)
-        {
-            List<int> scores =  new List<int>();
-            List<string> answers = new List<string>();
-            int roundCount = 0;
-            ChoHan game = new ChoHan();
 
-            int count;
-            while (roundCount > 5)
-            {
-
-                int answercount = 0;
-                //Waits for every client to choose a answer
-                while (answercount != players.Count)
-                {
-                    answercount = 0;
-                    foreach (var c in players)
-                    {
-                        if (SharedUtil.ReadMessage(c).Contains("True"))
-                        {
-                            answercount += 1;
-                            scores.Add(0);
-                        }
-                    }
-                }
-                //TODO Displays the result of the throw and annouces win or lose.
-                //send every client a message that they can send their answer
-                game.ThrowDice();
-                count = 0;
-                foreach (var c in players)
-                {
-                    SharedUtil.SendMessage(c, "1");
-                    string answer = (SharedUtil.ReadMessage(c));
-                    string[] message;
-                    if (answer.Equals("True"))
-                    {
-                        if (game.CheckResult(true))
-                        {
-                            scores.Insert(count, scores.ElementAt(count) + 1);
-                            message = new[] { scores.ElementAt(count).ToString(), "True"};
-                        }
-                        else
-                        {
-                            message = new[] { scores.ElementAt(count).ToString(), "False" };
-                        }
-                       SharedUtil.SendMessages(c, message);
-                    }
-                    else
-                    {
-                        if (game.CheckResult(false))
-                        {
-                            scores.Insert(count, scores.ElementAt(count) + 1);
-                            message = new[] { scores.ElementAt(count).ToString(), "True" };
-                        }
-                        else
-                        {
-                            message = new[] { scores.ElementAt(count).ToString(), "False" };
-                        }
-                      SharedUtil.SendMessages(c,message);
-                    }
-                    count++;
-
-
-                }
-                roundCount++;
-            }
-
-            //scores.Sort();
-
-            //TODO: Has to work for more clients.
-            switch (scores.ElementAt(0) - scores.ElementAt(1))
-            {
-                case 1:
-                    SharedUtil.SendMessage(players.ElementAt(0), "You win");
-                    SharedUtil.SendMessage(players.ElementAt(1), "You lose");
-                    break;
-
-                case 0:
-                    SharedUtil.SendMessage(players.ElementAt(0), "You tied");
-                    SharedUtil.SendMessage(players.ElementAt(1), "You tied");
-                    break;
-
-                case -1:
-                    SharedUtil.SendMessage(players.ElementAt(1), "You win");
-                    SharedUtil.SendMessage(players.ElementAt(0), "You lose");
-                    break;
-            }
-                //TODO also needs reworking. The room doesn't play with one player and only closes when the server shuts off.
-                foreach (var c in players)
-            {
-                SharedUtil.WriteTextMessage(c, "Thanks for playing.");
-            }
-
-            foreach (var c in players)
-            {
-                c.Close();
-            }
-        }
 
         public static IPAddress GetLocalIpAddress()
         {
@@ -165,16 +70,5 @@ namespace ChoHan
             throw new Exception("Local IP Address Not Found!");
         }
 
-        private void HandleClientThread()
-        {
-            StartGame(clients);
-            Console.WriteLine("Connection closed");
-        }
-
-        public int Compare(int x, int y)
-        {
-            return x - y;
-            
-        }
     }
 }
