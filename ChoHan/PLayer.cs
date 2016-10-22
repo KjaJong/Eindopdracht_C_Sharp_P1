2a628e78a93ea82744292f3849b395b8a1b1d31a
Gijs San <gijs.brian.joost@hotmail.com>
2016-10-22 13:07:57 +0200

    added player in server

---
+++ ChoHan/PLayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoHan
+{
+    class Player
+    {
+        public TcpClient Client { get; set; }
+        public string Naam { get; set; }
+        public int Score { get; set; }
+
+        public Player(string naam, TcpClient client, int score)
+        {
+            Naam = naam;
+            Client = client;
+            Score = score;
+        }
+    }
+}
