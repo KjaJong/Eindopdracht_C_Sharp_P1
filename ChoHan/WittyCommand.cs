937acdf8e22f05c686e7e1b868c649623e9722ee
gijsb_000 <gijsb_000@GIJS_BIEZELAAR>
2016-11-03 21:50:50 +0100

    Github plz

---
+++ ChoHan/WittyCommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChoHan
+{
+    public class WittyCommand
+    {
+        public static string WrongAnswer()
+        {
+            Random random = new Random();
+            var strings = new List<string> { "Be ashamed of yourself", "You disgust me", "DIPSHIT!!", "My cat can do better than you", "Having a bad time?" };
+            int index = random.Next(strings.Count);
+            var text = strings[index];
+            return text;
+        }
+
+        public static string GoodAnswer()
+        {
+            Random random = new Random();
+            var strings = new List<string> { "You expect a cookie or something?", "*Slow clap*", "You did kinda good", "Good job wasting your time on this game", "*Pat on the back*", "Lucky guess" };
+            int index = random.Next(strings.Count);
+            var text = strings[index];
+            return text;
+        }
+
+        public static string Win()
+        {
+            Random random = new Random();
+            var strings = new List<string> { "Cookie acquired.... Not", "We have a winner", "You win, now go outside", "You won...... NOTHING" };
+            int index = random.Next(strings.Count);
+            var text = strings[index];
+            return text;
+        }
+
+        public static string Lose()
+        {
+            Random random = new Random();
+            var strings = new List<string> { "You dishonor the family", "Looooosssseeeer!!!!", "Why so sad?" };
+            int index = random.Next(strings.Count);
+            var text = strings[index];
+            return text;
+        }
+
+        public static string Idle()
+        {
+            Random random = new Random();
+            var strings = new List<string> { "Dont idle dipshit", "AFK Loser", "Youre making everybody wait", "Youre worse than the losers" };
+            int index = random.Next(strings.Count);
+            var text = strings[index];
+            return text;
+        }
+
+        public static string Tied()
+        {
+            Random random = new Random();
+            var strings = new List<string> { "You manage to even the odds", "Try a little harder next time", "Tied, meh", "At least you didnt lose" };
+            int index = random.Next(strings.Count);
+            var text = strings[index];
+            return text;
+        }
+    }
+}
